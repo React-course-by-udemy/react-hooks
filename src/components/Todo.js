6508a57d5de4d45e900d299d06a3cebc75a00d7c
Adam Morsi <Adam.Morsi@mindgeek.com>
2019-02-11 14:51:27 +0200

    useEffect hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const todo = props => {
     // const inputState = useState('');
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://reacthooks-69e5d.firebaseio.com/todos.json')
+            .then(result => {
+                console.log(result);
+                const todoData = result.data;
+                let todos = [];
+                for (const key in todoData) {
+                    todos.push({id: key, name: todoData[key].name})
+                }
+                setTodoList(todos);
+            })
+            .catch(error => console.log(error));
+
+        return () => {
+            console.log('cleanup');
+        };
+    }, [todoName]);
 
     const inputChangedHandler = (event) => {
         setTodoName(event.target.value)
@@ -11,6 +30,9 @@ const todo = props => {
 
     const todoAddHandler = () => {
         setTodoList(todoList.concat(todoName));
+        axios.post('https://reacthooks-69e5d.firebaseio.com/todos.json', {name: todoName})
+            .then(res => console.log(res))
+            .catch(error => console.log(error));
     };
     return (
         <React.Fragment>
@@ -21,7 +43,7 @@ const todo = props => {
             <button type="button" onClick={todoAddHandler}>Add</button>
             {todoName}
             <ul>
-                {todoList.map(todo => <li key={todo}>{todo}</li>)}
+                {todoList.map(todo => <li key={todo.id}>{todo.name}</li>)}
             </ul>
         </React.Fragment>
     );
